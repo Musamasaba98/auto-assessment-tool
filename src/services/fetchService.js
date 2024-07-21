e4d970fd1dd046b7e7625b971022625b379df83d
Musamasaba98 <masamata98@gmail.com>
2024-07-21 11:16:55 -0700

    Fix: error withing the fetch service

--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -20,6 +20,7 @@ const fetchService = async (url) => {
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     );
+    await page.screenshot({ path: "screenshot.png" });
     const cssRules = await extractCSSRules(page, url, response);
     const htmlContent = await page.content();
     await browser.close();
